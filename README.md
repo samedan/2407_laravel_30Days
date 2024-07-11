5095d8d2c0d4a008dab5f6d79ec744b41099bf1e
Daniel Popescu <same.dan@gmail.com>
2024-07-11 13:04:59 +0200

    Add Model & migration,

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Talwind free componenents
 
 > https://tailwindui.com/components/application-ui/application-shells/stacked
+
+## Add Model & Migration
+
+> php artisan make:model Post -m
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model {
         protected $table = 'job_listings'; // reads job_listings Table column
+
+
+        protected $fillable = ['title', 'salary'];
 }
